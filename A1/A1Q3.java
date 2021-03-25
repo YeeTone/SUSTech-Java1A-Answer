3b4ab8f4068e43e21bd9f21ff484ce754469e32d
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-03-25 10:20:55 +0800

    Add files via upload

---
+++ A1/A1Q3.java
@@ -0,0 +1,28 @@
+package A1;
+
+import java.util.Scanner;
+
+public class A1Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h1 = sc.nextInt(), m1 = sc.nextInt(), s1 = sc.nextInt();
+        int h2 = sc.nextInt(), m2 = sc.nextInt(), s2 = sc.nextInt();
+
+        int allSecondsDiff = (h2 - h1) * 3600 + (m2 - m1) * 60 + (s2 - s1);
+
+        int minutes = allSecondsDiff / 60;
+        int seconds = allSecondsDiff - minutes * 60;
+
+        if(allSecondsDiff==0){
+            System.out.println("0s");
+        }else if(minutes==0){
+            System.out.println(seconds+"s");
+        }else if(seconds==0){
+            System.out.println(minutes+"m");
+        }else {
+            System.out.println(minutes+"m"+seconds+"s");
+        }
+        sc.close();
+    }
+}
---
+++ A1/A1Q4.java
@@ -0,0 +1,31 @@
+package A1;
+
+import java.util.Scanner;
+
+public class A1Q4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+
+        int sum=0,max=0;
+        for (int i = 0; i < t; i++) {
+            int value=sc.nextInt();
+            sum+=value;
+            max=Math.max(max,value);
+        }
+
+        String level;
+        if((t>=10&&sum>=3000)||max>=5000){
+            level="Diamond";
+        }else if((t>=5&&sum>=2000)||max>=3000){
+            level="Gold";
+        }else if((t>=3&&sum>=1000)||max>=1500){
+            level="Silver";
+        }else {
+            level="Ordinary";
+        }
+
+        System.out.println(level);
+        sc.close();
+    }
+}
