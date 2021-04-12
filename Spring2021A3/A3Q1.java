d539c15f48068d39c68510ed2123822d77f61ac4
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-04-12 10:11:16 +0800

    Update A3Q1.java

--- Spring2021A3/A3Q1.java
+++ Spring2021A3/A3Q1.java
@@ -1,12 +1,7 @@
-package Spring2021A3;
-
-import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class A3Q1 {
     public static void main(String[] args) {
-        BigDecimal d=new BigDecimal("1.3");
-        System.out.println(d.toEngineeringString());
         Scanner sc=new Scanner(System.in);
 
         int m=sc.nextInt();
@@ -21,9 +16,9 @@ public class A3Q1 {
 
         int[][]kaleidoscope=new int[2*m][2*n];
         for (int i = 0; i < m; i++) {// up -> down
-            // left -> right
-            if (n >= 0){
-                System.arraycopy(originMatrix[i], 0, kaleidoscope[i], 0, n);
+            for (int j = 0; j < n; j++) {
+                // left -> right
+                kaleidoscope[i][j]=originMatrix[i][j];
             }
 
             for (int j = 2*n-1; j >= n; j--) {
@@ -33,9 +28,9 @@ public class A3Q1 {
         }
 
         for (int i = 2*m-1; i >= m; i--) {
-            // left -> right
-            if (n >= 0){
-                System.arraycopy(originMatrix[2 * m - 1 - i], 0, kaleidoscope[i], 0, n);
+            for (int j = 0; j < n; j++) {
+                // left -> right
+                kaleidoscope[i][j]=originMatrix[2*m-1-i][j];
             }
 
             for (int j = 2*n-1; j >= n; j--) {
