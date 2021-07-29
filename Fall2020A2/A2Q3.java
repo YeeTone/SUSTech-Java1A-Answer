f9ad07f4e8f7fde1b106436e30e61ac977ebcf73
YeeTone <64548919+YeeTone@users.noreply.github.com>
2021-07-30 00:04:20 +0800

    Add files via upload

--- Fall2020A2/A2Q3.java
+++ Fall2020A2/A2Q3.java
@@ -12,14 +12,16 @@ public class A2Q3 {
 
         long largest = -1;
         long smallest = Long.MAX_VALUE - 7654321;
+        long mod = 998244353;
+
         for (int i = 0; i < n; i++) {
             lines[i] = sc.nextLine();
 
             long l = lines[i].length();
             long freq = 0, l_n =1;
-            for (int j = 0; j < l; j++, l_n*=l, l_n %= 998244353) {
-                freq += ((long) lines[i].charAt(j)%998244353) * (l_n%998244353);
-                freq %= 998244353;
+            for (int j = 0; j < l; j++, l_n*=l, l_n %= mod) {
+                freq += ((long) lines[i].charAt(j)%mod) * (l_n%mod);
+                freq %= mod;
             }
 
             largest = Math.max(largest,freq);
