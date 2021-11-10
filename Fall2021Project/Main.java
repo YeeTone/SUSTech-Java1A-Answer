560e2aad293d64fd6a4123bb32233252d72ebf33
yitong <yitongcool@foxmail.com>
2021-11-10 19:37:23 +0800

    更新2021Fall的project

---
+++ Fall2021Project/Main.java
@@ -0,0 +1,15 @@
+package Fall2021Project;
+
+import Fall2021Project.view.GameFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            GameFrame mainFrame = new GameFrame(800);
+            mainFrame.setVisible(true);
+        });
+    }
+}
---
+++ Fall2021Project/components/BasicComponent.java
@@ -0,0 +1,19 @@
+package Fall2021Project.components;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public abstract class BasicComponent extends JComponent {
+    public BasicComponent() {
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                onMouseClicked();
+            }
+        });
+    }
+
+    public abstract void onMouseClicked();
+}
