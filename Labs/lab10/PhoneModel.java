a7b82f16c438cf9afc792682cfdd3e07188c8550
yitong <yitongcool@foxmail.com>
2023-04-21 21:58:36 +0800

    lab10

---
+++ Labs/lab10/PhoneModel.java
@@ -0,0 +1,13 @@
+public enum PhoneModel {
+    IPHONE(9999), HUAWEI(8888), PIXEL(6666),
+    SAMSUNG(9399), LG(5588);
+    private int price;
+
+    PhoneModel(int price){
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ Labs/lab10/PhoneModelTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PhoneModelTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your budget: ");
+        int budget = sc.nextInt();
+
+        boolean anyoneFit = false;
+        for (PhoneModel pm: PhoneModel.values()){
+            if(pm.getPrice() <= budget){
+                if(!anyoneFit){
+                    System.out.println("---Recommended Phone List---");
+                    anyoneFit = true;
+                }
+                System.out.printf("%-16sprice: %d%n", pm.name(), pm.getPrice());
+            }
+        }
+
+        if(!anyoneFit){
+            System.out.println("You do not have sufficient money.");
+        }
+
+        sc.close();
+    }
+}
